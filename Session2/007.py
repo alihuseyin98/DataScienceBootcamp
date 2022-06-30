5c0b472724204687c802fda1837ac03967e985ab
alsein98 <ali.huseyinn98@gmail.com>
2022-06-30 19:41:20 +0300

    add

--- Session2/007.py
+++ Session2/007.py
@@ -4,6 +4,12 @@ customers=[["hasan",[20,40,50,60]],["alma",[40,30]],["ali",[70,10,20]],["ziad",[
 #1 
 payments=list(map(lambda c : sum(c[1]),customers))
 print("customer who paid more :",customers[payments.index(max(payments))][0])
-#print(list(map(lambda c : sum(c[1]),customers)))
+
 #2
 l2=sorted(customers, key=lambda c:sum(c[1]))
+print(l2[-1][0])
+#total payments made by all customers :
+
+#summ=0
+#ll=[sum(c[1]) for c in customers]
+print("total payments :",sum([sum(c[1]) for c in customers]))
