7c3cbcd75860c98e3ac924f52fa334842575be71
alsein98 <ali.huseyinn98@gmail.com>
2022-06-30 18:44:35 +0300

    uodate-30.06

--- Session2/006.py
+++ Session2/006.py
@@ -6,12 +6,20 @@ print(list("even" if x%2==0 else "odd" for x in range(10)))
 l2=[]
 for n in names :
     l2.append(n.upper()) 
-print(l2)
+print("#1 ",l2)
 #2
 l2=[]
 count = 0
 while (count<len(names)):
     l2.append(names[count].upper())
+    count+=1
+print("#2 ",l2)
 #3
 l2=[]
 l2=[n.upper() for n in names ]
+print("#3 ",l2)
+
+#4
+l2=[]
+l2=list(map(lambda n:n.upper(),names))
+print("#4 ",l2)
