fcfa30b31e3e3a620d3b7dc5bc0f0409a116cd1e
alsein98 <ali.huseyinn98@gmail.com>
2022-07-04 02:30:48 +0300

    03.07.2022

--- Session2/007.py
+++ Session2/007.py
@@ -3,14 +3,18 @@ customers=[["hasan",[20,40,50,60]],["alma",[40,30]],["ali",[70,10,20]],["ziad",[
 #name of the customer who paid more :
 #1 
 payments=list(map(lambda c : sum(c[1]),customers))
-print("customer who paid more :",customers[payments.index(max(payments))][0])
+print("#1 customer who paid more :",customers[payments.index(max(payments))][0])
 
 #2
 print("Sorted",sorted(customers, key=lambda c:sum(c[1]))[-1][0])
 l2=sorted(customers, key=lambda c:sum(c[1]))
-print("$2",l2[-1][0])
-#total payments made by all customers :
+print("#2",l2[-1][0])
 
+
+#total payments made by all customers :
 #summ=0
 #ll=[sum(c[1]) for c in customers]
-print("total payments :",sum([sum(c[1]) for c in customers]))
+#1
+print("#1 total payments :",sum([sum(c[1]) for c in customers]))
+#2
+print("#2 ",sum(list(map(lambda c : sum(c[1]),customers))))
---
+++ Session2/008.py
@@ -0,0 +1,5 @@
+names=['hasan', 'ziad', 'mike', 'alma', 'noor', 'sedra', 'william','jo']
+values=[10, 20, 4, 30, 60, 40, 50, 100]
+#zip
+ziped=list(zip(names,values))
+print(ziped)
